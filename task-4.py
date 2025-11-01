5c2f31b98f2b8ff9d19d8667f86fd9637aba4412
doston <dost7ony6@gmail.com>
2025-10-31 20:54:47 -0400

    changes you made

---
+++ task-4.py
@@ -0,0 +1,11 @@
+import random
+
+prizes = ["computer", "iphone", "headphones", "tablet", "gift card"]
+
+collected_prizes = []
+
+for i in range(3):
+    prize = random.choice(prizes)
+    collected_prizes.append(prize)
+
+print("Collected prizes:", collected_prizes)
