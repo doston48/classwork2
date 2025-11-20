e8ef852eab1ab17da7bab7009c79eeb93fc5afd9
doston <dost7ony6@gmail.com>
2025-11-19 20:44:37 -0500

    changes you made

---
+++ task-10.py
@@ -0,0 +1,9 @@
+
+class Student:
+    def _init_ (self, student):
+       self. student = student
+student = Student(["alice", "john"])
+def show_students(self):
+          for student in self.students:
+              print(student)
+         
---
+++ task-11.py
@@ -0,0 +1,18 @@
+class ShoppingCart:
+ def init_(self, item, price, items):
+  self.item = item
+  self.item = price
+  self.items = items
+
+ def add_item(self):
+  self.items [self.item] = self.price
+ 
+ def total_price(self):
+  for item, price in self.items:
+
+   def total_price(self) :
+    total = sum(self.price)
+    total = sum(self.prince.value())
+
+
+
